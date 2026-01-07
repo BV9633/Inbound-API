ddd969c0c9ca5c83e6bdff6c64dc4abcbf829057
CM4165 <CM4165@zebra.com>
2026-01-07 11:06:18 +0530

    deletion of unwanted files

--- CBP_API/schemas/cancel_schema.py
+++
@@ -1,6 +0,0 @@
-
-from pydantic import BaseModel
-
-class Cancel_CBP(BaseModel):
-    cbp_id:str
-    status:str
--- Waybill_API/timestamp.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime,timedelta,timezone
-
-def get_timestamp():
-    CST=timezone(timedelta(hours=-6))
-    now=datetime.now(CST)
-    mon=now.strftime("%b").lower().capitalize()
-    time= f"{now:%d}-{mon}-{now:%Y} {now:%H}:{now:%M}:{now:%S} CST"
-    return time
